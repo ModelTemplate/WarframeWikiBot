92f2bed0fc2ef575e239fa0bdb41c829e20fb254
Thomas <thmsphung2000@gmail.com>
2021-02-26 17:14:45 -0800

    Getting revisions from the past week

--- scripts/commit_to_repo.py
+++ scripts/commit_to_repo.py
@@ -6,44 +6,26 @@ Must be logged in to run; type python pwb.py login
 """
 import pywikibot
 import urllib
+from datetime import datetime, timedelta
 
 def get_watched_pages(site):
-    watchlist = fetch_watchlist(site)
+    time = (datetime.utcnow()- timedelta(days=7)).isoformat()
+    print(time)
+    watchlist = site.watchlist_revs(end=time, namespaces='0|8|10|14|828')
     for page in watchlist:
         print(page)
 
-    site.watchlist_revs(namespaces='0|8|10|14|828')
-
-
-def fetch_watchlist(site):
-    # Fetch the watchlist.
-    pywikibot.output('Retrieving watchlist for {0}.'.format(str(site)))
-    watched_pages = list(site.watched_pages(force=True))
-    return filter(filter_talk_page, watched_pages)
-
-
-def filter_talk_page(page):
-    # Filters talk pages
-    talk_pages = ['User talk', 'Category talk', 'Module talk']
-    if (page.namespace() in talk_pages):
-        return False
-    else:
-        return True
+def create_pull_request():
 
 
 def main(*args):
     """
     Process command line arguments and invoke bot.
 
-    If args is an empty list, sys.argv is used.
-
     @param args: command line arguments
     @type args: str
     """
     site = pywikibot.Site()
-    for key in site.namespaces:
-        print(str(key) + ": " + str(site.namespaces[key]))
-
     get_watched_pages(site)
 
 
